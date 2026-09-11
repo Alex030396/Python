a3fb5ccebe525fd688ab4bcec6b559db8e76e104
Alex030396 <albrix@gmail.com>
2026-09-11 16:52:50 -0400

    Python 7

---
+++ 07_dict.py
@@ -0,0 +1,43 @@
+# Dictionaries
+
+my_dict = dict()
+my_other_dict = {}
+
+print(type(my_dict))
+print(type(my_other_dict))
+
+my_other_dict = {"Nombre":"Alex", "Edad":30, "Apellido":"Briceño", 1 : "Python"}
+print(my_other_dict)
+
+my_dict = {
+    "Nombre":"Alex", 
+    "Edad":30, 
+    "Apellido":"Briceño", 
+    "Lenguajes" : {"Python","SQL","Dax"}
+    }
+
+print(my_dict)
+
+print(len(my_dict))
+print(len(my_other_dict))
+
+print(my_dict["Lenguajes"])
+my_dict["Edad"] = 31
+print(my_dict["Edad"])
+print(my_dict)
+
+my_dict["Vehiculo"] = "Moto BR200"
+print(my_dict)
+
+del my_dict["Vehiculo"]
+print(my_dict)
+
+print("Nombre" in my_dict)
+print("Alex" in my_dict)
+
+print(my_dict.items())
+print(my_dict.keys())
+print(my_dict.values())
+
+my_new_dict = my_dict.fromkeys(("Nombre", "Edad"))
+print(my_new_dict)
