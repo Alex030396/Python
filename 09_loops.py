64fd680083a5b7e3d56666d627a3846f4f1c8932
Alex030396 <albrix@gmail.com>
2026-09-14 16:58:20 -0400

    Python 9

---
+++ 09_loops.py
@@ -0,0 +1,19 @@
+# LOOPS
+
+# WHILE
+
+my_condition = 0
+
+while my_condition < 10:
+    print(my_condition)
+    my_condition += 1
+else:
+    print("Mi condicion es mayor o igual a 10")
+    
+while my_condition < 10:
+    print(my_condition)
+    my_condition += 2
+if my_condition == 10:
+    print("Mi condicion es igual a 10")
+else:
+    print("Mi condicion es mayor o igual a 10")
