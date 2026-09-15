9ef4b5ff9ef9cb495046f038f99654c8e5a583dc
Alex030396 <albrix@gmail.com>
2026-09-15 10:25:28 -0400

    Python 13

---
+++ 11_classes.py
@@ -0,0 +1,42 @@
+### CLASSES ###
+
+class MyEmptyPerson:
+    pass
+
+print(MyEmptyPerson)
+print(MyEmptyPerson())
+
+class Person:
+    def __init__(self,name, surname):
+        self.name = name
+        self.surname = surname
+
+my_person = Person("Alex","Briceno")
+print(my_person)
+print(my_person.name)
+print(my_person.surname)
+print(f"{my_person.name} {my_person.surname}")
+
+
+class Person1:
+    def __init__(self,name, surname):
+        self.full_name = f"{name} {surname}"
+        
+my_person = Person1("Alex","Briceño")
+print(my_person.full_name)
+
+class Person2:
+    def __init__(self,name, surname,alias = "No Alias"):
+        self.full_name = f"{name} {surname} {alias}"
+        self.__name = name # Propiedad Privada
+    def get_name(self):
+        return self.__name
+    def walk(self):
+        print(f"{self.full_name} Esta Caminando")
+
+my_person = Person2("Alex J.","Briceño H.","Albrix")
+print(my_person.full_name)
+my_person.walk()
+my_person.full_name = "Victoria Quesada (Mi amor Bello)"
+print(my_person.full_name)
+print(my_person.get_name())
