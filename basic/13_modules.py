de1b2b2f9a2b527fd94e833ca2f22729f4a83745
Alex030396 <albrix@gmail.com>
2026-09-15 11:56:34 -0400

    Python 15

---
+++ basic/13_modules.py
@@ -0,0 +1,22 @@
+# MODULES
+
+import module 
+module.sumValues(1,2,3)
+module.printvalue(336)
+
+from module import sumValues, printvalue
+sumValues(1,2,3)
+printvalue("Hola Alex José")
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib as plot
+import math
+
+print(math.pi)
+print(math.pow(5,2))
+
+from math import pi as pita
+
+print(pita)
---
+++ basic/module.py
@@ -0,0 +1,5 @@
+def sumValues(numberOne, numberTwo, numberThree):
+    print(numberOne + numberTwo + numberThree)
+    
+def printvalue(values):
+    print(values)
