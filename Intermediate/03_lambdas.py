b07c05a53660eea1bd04746dff5b2e89a0f7faeb
Alex030396 <albrix@gmail.com>
2026-09-16 11:17:29 -0400

    Python 20

---
+++ Intermediate/03_lambdas.py
@@ -0,0 +1,12 @@
+### LAMBDAS ###
+
+sum_two_values = lambda first_value, second_value: first_value + second_value
+print(sum_two_values(2,8))
+
+multiply_values = lambda  firts_values , second_values : firts_values * second_values + 5
+print(multiply_values(2,8))
+
+def sum_three_values(value):
+    return lambda first_value, second_value: first_value + second_value + value
+
+print(sum_three_values(5)(2,3))
