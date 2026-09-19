64a151e3a0278f6146676345b014b6fb33eb4368
alex030396 <albrix336@gmail.com>
2026-09-19 19:47:30 -0400

    Python 24

--- Intermediate/05_error_types.py
+++ Intermediate/05_error_types.py
@@ -25,3 +25,22 @@ print(math.pi)
 my_dict = {"Name":"Alex","Surname":"Briceño","Age":30}
 # print(my_dict["Ages"]) KeyError
 print(my_dict["Age"])
+
+# TypeError
+# print(my_list["Nombre"]) TypeError
+print(my_list[0]) 
+
+# ImportError
+# from math import PI ImportError
+from math import pi
+print(pi)
+
+# ValueError
+# my_int = int("10 años") ValueError
+my_int = int("10")
+print(type(my_int))
+
+# ZeroDivisionError
+# print(4/0) ZeroDivisionError
+print(4/2)
+
